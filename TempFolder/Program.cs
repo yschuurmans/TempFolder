b2233b0f431ef9a688facf50daed1bdca386e744
Youri Schuurmans <Youri.Schuurmans@InfoSupport.com>
2020-02-26 14:32:49 +0100

    Revamp

--- TempFolder/Program.cs
+++ TempFolder/Program.cs
@@ -9,19 +9,60 @@ using System.Threading.Tasks;
 
 namespace TempFolder
 {
-    class Program
+    static class Program
     {
+        private static int DAYS_TO_PRESERVE = 10;
+        private static int FOLDERS_TO_PRESERVE = 10;
+
         static void Main(string[] args)
         {
-            string dateName = DateTime.Now.ToString("yyyyMMdd");//Path.Combine("%TEMP\\", DateTime.Now.ToLongDateString());
-            
-            if (!Directory.Exists(Path.Combine(Path.GetTempPath(), dateName)))
-            {
-                Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), dateName));
-            }
-            Process.Start(Path.Combine(Path.GetTempPath(), dateName));
+            DateTime date = DateTime.Now;
+            var existed = CreateFolder(date);
+            Process.Start(GetPath(date));
 
+            if (!existed)
+                RemoveOldFolders(DateTime.Now);
             Environment.Exit(0);
         }
+
+        public static string GetPath(this DateTime date)
+        {
+            return Path.Combine(Path.GetTempPath(), "_TEMP_", date.ToString("yyyyMMdd"));
+        }
+
+        public static bool CreateFolder(DateTime date)
+        {
+            if (!Directory.Exists(GetPath(date)))
+            {
+                Directory.CreateDirectory(GetPath(date));
+                return false;
+            }
+            return true;
+        }
+
+        public static void RemoveOldFolders(DateTime currentDate)
+        {
+            int removedFolders = 0;
+            for (int i = DAYS_TO_PRESERVE; i < 365; i++)
+            {
+                if(RemoveFolder(currentDate.AddDays(-i)))
+                    removedFolders++;
+                
+                if(removedFolders >= FOLDERS_TO_PRESERVE)
+                    break;
+            }
+        }
+
+        public static bool RemoveFolder(DateTime date)
+        {
+            if (!Directory.Exists(GetPath(date)))
+                return false;
+
+            Directory.Delete(GetPath(date), true);
+            RemoveOldFolders(DateTime.Now);
+            return true;
+        }
+
+
     }
 }
